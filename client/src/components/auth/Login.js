bd162c13d43b0bd3bd99903909691af43a6497d1
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-04 20:12:32 -0400

    add styling to form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-
+import Form from 'react-bootstrap/Form';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 class Login extends React.Component {
     render() {
         return(
-            <>
-            
-            <Container>
-                <Row>
+            <Form noValidate>
+                <FormGroup controlId="formBasicEmail">
+                    <FormLabel>Email address</FormLabel>
+                    <FormControl type="email" placeholder="Enter email" />
                     
-                </Row>
-            </Container>
-            </>
+                </FormGroup>
+
+                <FormGroup controlId="formBasicPassword">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl type="password" placeholder="Password" />
+                </FormGroup>
+
+                <Button variant="info" type="submit">
+                    Submit
+                </Button>
+            </Form>
         )
     }
 }
